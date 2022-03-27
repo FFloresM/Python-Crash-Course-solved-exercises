5bed8cf55ecd9eaf278e2644daff9c60920dca74
Francisco <victoriafrancis2009@gmail.com>
2022-03-27 00:03:26 -0300

    comments

---
+++ Chapter 2/2-10.adding_comments.py
@@ -0,0 +1,6 @@
+#variable to save my favorite number
+favorite_number = 28
+#variable to create a string with a sentence and my favorite number 
+message = f"my favorite number is {favorite_number}"
+#print the message
+print(message)
