e849e9edaa8e0ce464fa725627b44743f1162381
Francisco <victoriafrancis2009@gmail.com>
2022-03-27 11:26:50 -0300

    guest list

---
+++ Chapter 3/3-4.guest_list.py
@@ -0,0 +1,5 @@
+guest_list = ['Walter White', 'Barack Obama', 'Alien']
+invitation = "Let me invite you to my dinner Mr "
+print(invitation + guest_list[0])
+print(invitation + guest_list[1])
+print(invitation + guest_list[2])
