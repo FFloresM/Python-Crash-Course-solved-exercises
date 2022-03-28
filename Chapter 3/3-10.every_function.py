3a7e22a080ea3fff767f706be45e04c3c74f9711
Francisco <victoriafrancis2009@gmail.com>
2022-03-28 19:02:28 -0300

    3-8 to 3-10

---
+++ Chapter 3/3-10.every_function.py
@@ -0,0 +1,8 @@
+list_of_things = ['everest', 'amazonas', 'santiago', 'germany', 'español', 'book']
+print(list_of_things)
+list_of_things.reverse()
+print(list_of_things)
+list_of_things.sort()
+print(list_of_things)
+print(sorted(list_of_things, reverse=True))
+print(f"size of list_of_things: {len(list_of_things)}")
---
+++ Chapter 3/3-9.dinner_guests.py
@@ -0,0 +1,6 @@
+guest_list = ['Walter White', 'Barack Obama', 'Alien']
+invitation = "Let me invite you to my dinner Mr "
+print(invitation + guest_list[0])
+print(invitation + guest_list[1])
+print(invitation + guest_list[2])
+print(f"Number of people invited: {len(guest_list)}")
