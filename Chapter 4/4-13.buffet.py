c162f9b37dd6c9e42bf33d74cc54fe3b95dfa66a
Francisco <victoriafrancis2009@gmail.com>
2022-04-11 12:40:48 -0400

    tuples

---
+++ Chapter 4/4-13.buffet.py
@@ -0,0 +1,9 @@
+foods = ('pasta', 'rice', 'gelly', 'potatoes', 'ice cream')
+for food in foods:
+    print(food)
+
+foods[2] = 'salad' #comment this line to change foods tuple
+
+foods = ('pasta', 'rice', 'salad', 'juice', 'ice cream')
+for food in foods:
+    print(food)
