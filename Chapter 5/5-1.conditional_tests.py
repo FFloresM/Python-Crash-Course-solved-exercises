69573eaf7ed80872b68656e1bf31ca47812fac29
Francisco <victoriafrancis2009@gmail.com>
2022-04-13 15:11:07 -0400

    conditionals

---
+++ Chapter 5/5-1.conditional_tests.py
@@ -0,0 +1,33 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+number = 10
+print("Is number > 10? I predict False.")
+print(number > 10)
+
+print("Is number positive? I predict True.")
+print(number > 0)
+
+print("Is number odd? I predict False.")
+print(number % 2 == 0)
+
+print("Is number even? I predict True.")
+print(number % 2 == 0)
+
+print("Is number divisible by 5? I predict True.")
+print(number % 5 == 0)
+
+print("Is number multiple of 3? I predict False.")
+print(number % 3 == 0)
+
+house = 'house'
+print("Is house == 'home'? I predict Fasle.")
+print(house == 'home')
+
+print("Is house a string? I predict True.")
+print(type(house))
+print(type(house) == 'str')
