3ba1e4ff5c565740365a93ef57ec6231d555ab2f
Francisco <victoriafrancis2009@gmail.com>
2022-04-18 18:33:58 -0400

    alien colors

---
+++ Chapter 5/4-5.alien_colors_2.py
@@ -0,0 +1,12 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("player just earned 5 points for shooting the alien")
+else:
+    print('player earned 10 points')
+
+if alien_color == 'red':
+    print("player just earned 5 points for shooting the alien")
+else:
+    print('player earned 10 points')
+
---
+++ Chapter 5/5-3.alien_colors.py
@@ -0,0 +1,10 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("player just earned 5 points")
+
+#onther version
+if alien_color == 'green':
+    print("player just earned 5 points")
+if alien_color == 'red':
+    print()
