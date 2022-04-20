4faa4e486c58486f1e6ce6359944f19fe02f4597
Francisco <victoriafrancis2009@gmail.com>
2022-04-20 12:46:53 -0400

    5-5 to 5-7

---
+++ Chapter 5/5-5.alien_colors_3.py
@@ -0,0 +1,10 @@
+alien_color = 'green'
+#alien_color = 'yellow'
+#alien_color = 'red'
+
+if alien_color == 'green':
+    print("player just earned 5 points for shooting the alien")
+elif alien_color == 'yellow':
+    print('player earned 10 points')
+else:
+    print('player earned 15 points')
---
+++ Chapter 5/5-7.favorite_fruit.py
@@ -0,0 +1,16 @@
+favorite_fruits = ['banana', 'melon', 'apple']
+# use of named expression from Python 3.8
+if (fruit:='banana') in favorite_fruits:
+    print(f"You really like {fruit}")
+
+if (fruit:='grapes') in favorite_fruits:
+    print(f"You really like {fruit}")
+
+if (fruit:='pear') in favorite_fruits:
+    print(f"You really like {fruit}")
+
+if (fruit:='melon') in favorite_fruits:
+    print(f"You really like {fruit}")
+
+if (fruit:='apple') in favorite_fruits:
+    print(f"You really like {fruit}")
